eb99fbf1814b65b7d82d86f5500cf4a1e7594c91
khanabdu25 <khanabdu@umich.edu>
2024-12-12 13:48:37 -0500

    init

---
+++ borrow-nest/Program.cs
@@ -0,0 +1,83 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using cutz_backend.Models;
+using cutz_backend.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<CutzContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("BNDatabase"),
+        npgsqlOptions => npgsqlOptions.CommandTimeout(300)
+    )
+// .EnableSensitiveDataLogging()
+// .LogTo(Console.WriteLine)
+);
+
+builder.Services.AddIdentity<CutzUser, IdentityRole>()
+    .AddEntityFrameworkStores<CutzContext>()
+    .AddDefaultTokenProviders();
+
+// Configure JWT Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+
+    var jwtKey = builder.Configuration["Jwt:Key"];
+    if (string.IsNullOrWhiteSpace(jwtKey))
+    {
+        throw new InvalidOperationException("JWT key is not configured properly.");
+    }
+
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(jwtKey)
+        )
+    };
+});
+
+// builder.Services.AddOpenApi();
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<RoleCheckerService>();
+
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var roleService = scope.ServiceProvider.GetService<RoleCheckerService>() ?? throw new InvalidOperationException("RoleCheckerService is not registered properly.");
+    await roleService.EnsureRolesAsync(roleManager);
+}
+
+if (app.Environment.IsDevelopment())
+{
+    //app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
