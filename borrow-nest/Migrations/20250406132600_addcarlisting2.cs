67337261f91c149143911bd6a95772a4d32fdb4c
khanabdu25 <khanabdu@umich.edu>
2025-04-06 09:26:44 -0400

    redo migration after adding to db context

---
+++ borrow-nest/Migrations/20250406132600_addcarlisting2.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace borrownest.Migrations
+{
+    /// <inheritdoc />
+    public partial class addcarlisting2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CarListings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Model = table.Column<string>(type: "text", nullable: false),
+                    Year = table.Column<int>(type: "integer", nullable: false),
+                    Mileage = table.Column<int>(type: "integer", nullable: false),
+                    Location = table.Column<string>(type: "text", nullable: false),
+                    PricePerDay = table.Column<decimal>(type: "numeric", nullable: false),
+                    Availability = table.Column<string>(type: "text", nullable: false),
+                    PickUpLocation = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarListings", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarListings");
+        }
+    }
+}
