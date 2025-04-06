9507b1ad8c3127d605413290795cca019f590e61
khanabdu25 <khanabdu@umich.edu>
2025-04-06 10:07:44 -0400

    update database after adding booking model and dtos

---
+++ borrow-nest/Migrations/20250406140635_addbooking.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace borrownest.Migrations
+{
+    /// <inheritdoc />
+    public partial class addbooking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReservedEndDate",
+                table: "CarListings",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReservedStartDate",
+                table: "CarListings",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SellerId",
+                table: "CarListings",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "CarListings",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CarId = table.Column<int>(type: "integer", nullable: false),
+                    RenterId = table.Column<string>(type: "text", nullable: true),
+                    OwnerId = table.Column<string>(type: "text", nullable: true),
+                    StartDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    TotalPrice = table.Column<decimal>(type: "numeric", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Bookings_AspNetUsers_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Bookings_AspNetUsers_RenterId",
+                        column: x => x.RenterId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Bookings_CarListings_CarId",
+                        column: x => x.CarId,
+                        principalTable: "CarListings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarListings_SellerId",
+                table: "CarListings",
+                column: "SellerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_CarId",
+                table: "Bookings",
+                column: "CarId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_OwnerId",
+                table: "Bookings",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_RenterId",
+                table: "Bookings",
+                column: "RenterId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CarListings_AspNetUsers_SellerId",
+                table: "CarListings",
+                column: "SellerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CarListings_AspNetUsers_SellerId",
+                table: "CarListings");
+
+            migrationBuilder.DropTable(
+                name: "Bookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CarListings_SellerId",
+                table: "CarListings");
+
+            migrationBuilder.DropColumn(
+                name: "ReservedEndDate",
+                table: "CarListings");
+
+            migrationBuilder.DropColumn(
+                name: "ReservedStartDate",
+                table: "CarListings");
+
+            migrationBuilder.DropColumn(
+                name: "SellerId",
+                table: "CarListings");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "CarListings");
+        }
+    }
+}
